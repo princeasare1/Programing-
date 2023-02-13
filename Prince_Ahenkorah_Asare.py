ba4da0ec0b506120594468c1014afe05caa65840
princeasare1 <125132159+princeasare1@users.noreply.github.com>
2023-02-13 09:57:01 +0000

    Add files via upload
    Multiplication Times Table with Python

---
+++ Prince_Ahenkorah_Asare.py
@@ -0,0 +1,8 @@
+Number= int(input("Enter number for the multiplication table: "))
+print("The Multiplication Table of:",Number)
+for count in range(1,13):
+    print(Number,"x",count,"=",Number*count)
+
+
+
+input("Press ENTER to Exit")
